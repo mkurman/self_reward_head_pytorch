d40bb578e1308149b03f89db52e77fded49d2268
mkurman <mariusz.kurman@gmail.com>
2024-06-18 19:28:20 +0200

    Code cleanup

--- head.py
+++ head.py
@@ -1,163 +1,132 @@
+from typing import Tuple, Any
+
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
+import random
 
-# --- This is just a fragment of the model, the most important part is reward_lm_head and its implementation in the forward() function --- #
-# --- You can find the LinearMoE implementation in my repository: https://github.com/mkurman/linearmoe_pytorch --- #
+from typing import Optional, Union
 
-class KurmanForCausalLM(KurmanPreTrainedModel):
-  _keys_to_ignore_on_load_missing = [r"lm_head.weight"]
 
-  def __init__(self, config: KurmanConfig):
-      super().__init__(config)
+class SelfRewardHead(nn.Module):
+    def __init__(self, in_features: int, out_features: int = 1, bias: bool = False):
+        super(SelfRewardHead, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
 
-      self.config = config
+        # You can use the LinearMoE implementation from my repository: https://github.com/mkurman/linearmoe_pytorch
+        self.reward_head = nn.Linear(in_features, out_features, bias=bias)
 
-      self.model = KurmanModel(config)
-      self.lm_head = LinearMoE(config.hidden_size, config.vocab_size,  bias=False, num_experts=config.num_experts, top_k=config.top_k_experts, r=config.experts_r)
-
-      # This is the most important layer. You can use a simple Linear layer instead.
-      self.rewarding_lm_head = LinearMoE(config.hidden_size, 1, bias=False, num_experts=config.num_experts, top_k=config.top_k_experts, r=config.experts_r)
-
-      self.reset_parameters()
-
-  def reset_parameters(self):
-      # ...
-
-  def forward(self,
-              input_ids: torch.LongTensor = None,
-              attention_mask=None,
-              labels: Optional[torch.LongTensor] = None,
-              past_key_values=None,
-              position_ids=None,
-              token_type_ids=None,
-              output_attentions=None,
-              output_hidden_states=None,
-              use_cache: Optional[bool] = None,
-              return_dict: Optional[bool] = None,
-              **kwargs,
-              ) -> Union[Tuple, RewardCausalLMOutputWithPast]:
-      return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-      outputs = self.model(
-          input_ids=input_ids,
-          attention_mask=attention_mask,
-          use_cache=use_cache,
-          return_dict=return_dict,
-          past_key_values=past_key_values,
-          **kwargs, )
-
-      hidden_state = outputs[0]
-
-      logits = self.lm_head(hidden_state)
-
-      if len(outputs) > 1:
-          past_key_values = outputs[1]
-  
-      loss = None
-                
-      reward_loss = None
-      reward_labels = None
-      reward_outputs = None
-                
-      if labels is not None:
-        # This is the most important part of the training phase.
-        # -> Preparing inputs for self reward
+    def forward(self, input_ids: torch.FloatTensor, output: Optional[torch.FloatTensor] = None) -> tuple[
+        torch.FloatTensor, torch.FloatTensor]:
         with torch.no_grad():
             reward_outputs = []
             reward_labels = []
             for i in range(input_ids.shape[0]):
                 output_to_choose = random.randint(0, 1)
-    
-                if output_to_choose == 0:
-                    reward_input = outputs[0].argmax(dim=-1)
+
+                if output is not None and output_to_choose == 0:
+                    reward_input = output.argmax(dim=-1)
                 else:
                     reward_input = input_ids
-  
+                    output_to_choose = 1
+
                 reward_input = self.model.embeddings(torch.stack([reward_input[i]]))[0]
-  
+
                 s, d = reward_input.size()
                 mask = torch.ones((s, s), dtype=torch.bool, device=reward_input.device).triu(1)
                 ones = torch.ones((s, s), dtype=reward_input.dtype, device=reward_input.device)
-        
+
                 summator = ones.masked_fill(mask, 0.)
-        
+
                 reward_input = summator @ reward_input
-                
+
                 reward_outputs.append(reward_input[..., -1:, :])
                 reward_labels.append(torch.tensor(float(output_to_choose)))
-  
-            reward_outputs = torch.stack(reward_outputs) 
+
+            reward_outputs = torch.stack(reward_outputs)
             reward_labels = torch.stack(reward_labels)
-  
+
         reward_output = self.rewarding_lm_head(reward_outputs)
 
-        # calc causal loss
-        loss_fct = nn.CrossEntropyLoss(reduction='mean')
-        
-        loss_logits = logits[..., :-1, :].contiguous()
-        loss_labels = labels[..., 1:].contiguous()
-        
-        loss_logits = loss_logits.view(-1, self.config.vocab_size)
-        loss_labels = loss_labels.view(-1)
-        
-        loss_labels = loss_labels.to(loss_logits.device)
-        loss = loss_fct(loss_logits, loss_labels)
+        return reward_output, reward_labels
 
-        # calc self reward loss
-        loss_fct = nn.BCEWithLogitsLoss(reduction='mean')
-        
-        loss_logits = reward_output[..., :].contiguous()
-        loss_labels = reward_labels[..., :].contiguous()
 
-        loss_logits = loss_logits.view(-1)
-        loss_labels = loss_labels.view(-1)
-        
-        loss_labels = loss_labels.to(loss_logits.device)
-        
-        reward_loss = loss_fct(loss_logits, loss_labels)
+class AnyModelForCausalLM(AnyModel):
+    _keys_to_ignore_on_load_missing = [r"lm_head.weight"]
 
-        # sum losses
-        loss = loss + reward_loss
+    def __init__(self, config: AnyConfig):
+        super().__init__(config)
 
-      if not return_dict:
-          output = (logits,) + outputs[1:]
-          return loss, output, past_key_values
+        # ... previous code
 
-      return RewardCausalLMOutputWithPast(
-          loss=loss,
-          logits=logits,
-          past_key_values=past_key_values,
-          reward_logits=reward_output,
-          reward_loss=reward_loss,
-          reward_labels=reward_labels,
-      )
+        # This is the most important layer. You can use a simple Linear layer instead.
+        self.rewarding_lm_head = SelfRewardHead(config.hidden_size, 1, bias=False)
 
-  # ...
+    def forward(self,
+                input_ids: torch.LongTensor = None,
+                attention_mask=None,
+                labels: Optional[torch.LongTensor] = None,
+                past_key_values=None,
+                position_ids=None,
+                token_type_ids=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                use_cache: Optional[bool] = None,
+                return_dict: Optional[bool] = None,
+                **kwargs,
+                ) -> Union[Tuple, Any]:
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-  # Self-rewarding method that allows you to rate any tokenized text
-  def calc_reward(self, input_ids):
-    with torch.no_grad():
-        reward_input = self.model.embeddings(input_ids)
-        
-        b, s, d = reward_input.size()
-        
-        mask = torch.ones((s, s), dtype=torch.bool, device=reward_input.device).triu(1)
-        ones = torch.ones((s, s), dtype=reward_input.dtype, device=reward_input.device)
-  
-        summator = ones.masked_fill(mask, 0.)
-  
-        reward_input = summator @ reward_input
-            
-        return F.sigmoid(self.rewarding_lm_head(reward_input[:, -1:]))
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            use_cache=use_cache,
+            return_dict=return_dict,
+            past_key_values=past_key_values,
+            **kwargs, )
 
-@dataclass
-class RewardCausalLMOutputWithPast(CausalLMOutputWithPast):
-  loss: Optional[torch.FloatTensor] = None
-  logits: torch.FloatTensor = None
-  past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
-  hidden_states: Optional[Tuple[torch.FloatTensor, ...]] = None
-  attentions: Optional[Tuple[torch.FloatTensor, ...]] = None
-  reward_logits: Optional[torch.FloatTensor] = None
-  reward_loss: Optional[torch.FloatTensor] = None
-  reward_labels: Optional[torch.FloatTensor] = None
-  
+        hidden_state = outputs[0]
+
+        logits = self.lm_head(hidden_state)
+
+        loss = None
+
+        if labels is not None:
+            # This is the most important part of the training phase.
+            reward_output, reward_labels = self.rewarding_lm_head(input_ids, outputs[0])
+
+            # calc causal loss
+            loss_fct = nn.CrossEntropyLoss(reduction='mean')
+
+            loss_logits = logits[..., :-1, :].contiguous()
+            loss_labels = labels[..., 1:].contiguous()
+
+            loss_logits = loss_logits.view(-1, self.config.vocab_size)
+            loss_labels = loss_labels.view(-1)
+
+            loss_labels = loss_labels.to(loss_logits.device)
+            loss = loss_fct(loss_logits, loss_labels)
+
+            # calc self reward loss
+            loss_fct = nn.BCEWithLogitsLoss(reduction='mean')
+
+            loss_logits = reward_output[..., :].contiguous()
+            loss_labels = reward_labels[..., :].contiguous()
+
+            loss_logits = loss_logits.view(-1)
+            loss_labels = loss_labels.view(-1)
+
+            loss_labels = loss_labels.to(loss_logits.device)
+
+            reward_loss = loss_fct(loss_logits, loss_labels)
+
+            # sum losses
+            loss = loss + reward_loss
+
+        # ... rest of the code
+
+    # Self-rewarding method that allows you to rate any tokenized text
+    def calc_reward(self, input_ids):
+        with torch.no_grad():
+            return F.sigmoid(self.rewarding_lm_head(input_ids)[0])
